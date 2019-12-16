95048780fc0e9adb818ca9f717c6ff59dfe8156a
Amir Ajorloo <amirajorloo@gmail.coml>
2019-12-16 13:57:55 +0330

    update redis url. add docker-compose

--- src/main/java/com/ajorloo/exchanger/Redis.java
+++ src/main/java/com/ajorloo/exchanger/Redis.java
@@ -6,7 +6,7 @@ public class Redis {
 
     public static void set(String key, String value, Integer ttl) {
 
-        Jedis jedis = new Jedis("localhost");
+        Jedis jedis = new Jedis("redis");
         jedis.set(key, value);
         if (ttl != null) {
             jedis.expire(key, ttl);
@@ -15,7 +15,7 @@ public class Redis {
     }
 
     public static String get(String key) {
-        Jedis jedis = new Jedis("localhost");
+        Jedis jedis = new Jedis("redis");
         String value = jedis.get(key);
         jedis.close();
         return value;
